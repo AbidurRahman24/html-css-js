e1cf60fd9b2ad03ecac7453b11682708e50c1417
Abidur Niloy <niloyabidur@gmail.com>
2023-11-24 22:27:46 +0600

    js project done

--- exam/assaignment 02/project.js
+++ exam/assaignment 02/project.js
@@ -8,12 +8,13 @@ const loadCategory = () => {
 const displayData = data => {
     // console.log(data);
     const CategoryContainer = document.getElementById("CategoryContainer");
+    CategoryContainer.innerHTML = '';
     data.forEach((cate) => {
         // console.log(cate);
         const catMenu = document.createElement("div");
         catMenu.innerHTML = `
-            <li class="nav-item ">
-            <a class="nav-link active" href="#" onclick="event.preventDefault(); displayProduct('${cate.category_id}')">${cate.category}</a>
+            <li class="nav-item btn btn-dark m-2">
+            <a class="nav-link text-white" href="#" onclick="displayProduct('${cate.category_id}')">${cate.category}</a>
             </li>
         
         `;
@@ -23,7 +24,6 @@ const displayData = data => {
 loadCategory()
 
 // product load
-
 const displayProduct = async (id) => {
     // console.log(id);
     try {
@@ -32,6 +32,11 @@ const displayProduct = async (id) => {
         );
         const data = await response.json();
         displayProductDetails(data.data);
+        if (id === '1005' || id != id) {
+            display404Data();
+        } else {
+            displayProductDetails(data.data);
+        }
     } catch {
         (err) => {
             console.log(err);
@@ -39,84 +44,81 @@ const displayProduct = async (id) => {
     }
 };
 
+displayProduct(1000)
 const displayProductDetails = (productList) => {
-    // Assuming productList is an array of product data objects
-    // console.log(productList);
 
     const displayContainer = document.getElementById("displayContainer");
+    displayContainer.innerHTML = '';
+
     function formatDuration(seconds) {
         const formattedTime = new Date(seconds * 1000).toISOString().substr(11, 8);
         return formattedTime;
     }
-    // Loop through each product in the productList
+    
     for (const product of productList) {
-        console.log(product);
+        // console.log(product);
         const cardHtml = `
-        
-    <div class="col-md-3 my-2">
-        <div class="card h-100 " style="width: 17rem;">
-            <img src="${product.thumbnail}" class="card-img-top" style="width: 100%; height: 250px; object-fit: cover;" alt="${product.title}">
-            <div class="card-body">
-                <div class="d-flex align-items-center">
-                    <img src="${product.authors[0].profile_picture}" style="width: 100px; height: 100px; padding:10px;" class="rounded-circle" alt="${product.authors[0].profile_name}">
-                    
-                    <div>
-                    <h5 class="card-title">${product.others.posted_date !== '' ? formatDuration(product.others.posted_date) : ''}</h5>
-                        <h5 class="card-title">${product.title}</h5>
-                        <div class="d-flex>
-                            <p class="card-text">${product.authors[0].profile_name}</p>
-                            <p>${product.authors[0].verified ? '<i class="fa-solid fa-circle-check" style="color: #0088ff;"></i>' : ''}</p>
+                    <div class="col-md-3 col-sm-12 mx-auto my-2">
+                        <div class="card h-100 " style="width: 17rem;">
+                            <img src="${product.thumbnail}" class="card-img-top" style="width: 100%; height: 250px; object-fit: cover;" alt="${product.title}">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <img src="${product.authors[0].profile_picture}" style="width: 100px; height: 100px; padding:10px;" class="rounded-circle" alt="${product.authors[0].profile_name}">
+                                    <div class="card-img-overlay ">
+                                    <div class="d-flex" >
+                                        <h5 class="card-title text-white bg-dark" style="display: inline; margin-right: 5px; align-self: flex-end;">${product.others.posted_date !== '' ? formatDuration(product.others.posted_date) : ''}</h5>
+                                    </div>
+                                    </div>
+                                    <div>
+                                    
+                                        <h5 class="card-title">${product.title}</h5>
+                                        <div class="d-flex">
+                                            <p class="card-text">${product.authors[0].profile_name}</p>
+                                            <p class=" px-3">${product.authors[0].verified ? '<i class="fa-solid fa-circle-check" style="color: #0088ff;"></i>' : ''}</p>
+                                        </div>
+                                        <p class="card-text">${product.others.views}</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <p class="card-text">${product.others.views}</p>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
         `;
-
-        // Create a temporary container element to convert the HTML string to DOM elements
         const tempContainer = document.createElement("div");
         tempContainer.innerHTML = cardHtml;
-
-        // Append the card to the displayContainer
         displayContainer.appendChild(tempContainer.firstElementChild);
     }
 };
+// 404 page
+const display404Data =(productList) =>{
+    console.log(productList);
+    const displayContainer = document.getElementById("displayContainer");
 
+    const card = document.createElement("div");
+    card.innerHTML = `
+            <div class="d-flex align-items-center justify-content-center" style="height: 80vh;">
+            <div class="mx-auto text-center" style="width: 10rem;">
+                <img src="./static/Icon.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Oops!! Sorry, There is no content here</h5>
+                </div>
+            </div>
+        </div>
 
-// sort function
+    `
+    displayContainer.appendChild(card);
+};
 
-// const sortByViews = async (id) => {
-//     // console.log(id);
-//     try {
-//         const response = await fetch(
-//             `https://openapi.programming-hero.com/api/videos/category/${id}`
-//         );
-//         const data = await response.json();
-//         displayProductDetails(data.data);
-//     } catch {
-//         (err) => {
-//             console.log(err);
-//         };
-//     }
-// };
-// function sortByViews() {
-    
-//     jsonData.data.sort((a, b) => {
-//       const viewsA = parseInt(a.others.views.replace('K', '')); // Convert views to a comparable number
-//       const viewsB = parseInt(b.others.views.replace('K', ''));
+const sortByViewsLink = document.getElementById('sortByViews');
+sortByViewsLink.addEventListener('click', async () => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/1000`);
+    const data = await response.json();
+        // console.log(data.data );
+        const convertedData = data.data.map(item => {
+            // console.log(item);
+            const views = parseFloat(item.others.views.replace('k', '')) *1000 ;
+            return { ...item, others: { ...item.others, views } };
+        });
+        const sortedData = convertedData.sort((a, b) => b.others.views - a.others.views);
 
-//       return viewsB - viewsA; // Sort in descending order
-//     });
-
-//     // Display or manipulate the sorted data as needed
-//     console.log(jsonData.data);
-//   }
-
-//   // Add an event listener to the "Sort By View" link
-//   document.getElementById('sortByViews').addEventListener('click', sortByViews);
-
-//   // Initial display of the unsorted data
-//   console.log(jsonData.data);
+        displayProductDetails(sortedData);
+});
